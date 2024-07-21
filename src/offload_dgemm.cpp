c887a614b04053dc783477d323d9f7d997a3e5c5
domke <673751-domke@users.noreply.gitlab.com>
2024-07-21 14:57:59 +0900

    del 2x init

--- src/offload_dgemm.cpp
+++ src/offload_dgemm.cpp
@@ -81,10 +81,10 @@ void offload_dgemm(const int oLayout,
     cublasOperation_t transb = (0 == oTransB) ? CUBLAS_OP_N : CUBLAS_OP_T;
     int m = oM, n = oN, k = oK, lda = oLda, ldb = oLdb, ldc = oLdc;
     double alpha[1] = { oAlpha }, beta[1] = { oBeta };
-    checkCudaErrors(
+//    checkCudaErrors(
             cublasDgemm_v2(handle, transa, transb, m, n, k,
-                           alpha, A, lda, B, ldb, beta, C, oLdc)
-            );
+                           alpha, A, lda, B, ldb, beta, C, oLdc);
+//            );
 
     checkCudaErrors(
             cudaMemcpy(oC, C, memSizeC, cudaMemcpyDeviceToHost)
